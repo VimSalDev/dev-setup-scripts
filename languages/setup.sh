9fd57dc66dfb83660b666e364e730818bd68fed4
miguel <miguel.santillan.yt@gmail.com>
2024-06-16 07:12:12 -0600

    Fix: Correct order of execution languages setup sript

--- languages/setup.sh
+++ languages/setup.sh
@@ -6,10 +6,10 @@ SCRIPT_DIR=$(dirname "$0")
 
 # Run all setup scripts
 "$SCRIPT_DIR/cpp-c/setup.sh"
-"$SCRIPT_DIR/go/setup.sh"
 "$SCRIPT_DIR/java/setup.sh"
 "$SCRIPT_DIR/node/setup.sh"
 "$SCRIPT_DIR/python/setup.sh"
+"$SCRIPT_DIR/go/setup.sh"
 
 # Final message
 echo "Process completed! Now you can program :)"
